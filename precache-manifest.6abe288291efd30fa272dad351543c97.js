e0274de81d61e582715401bd9bace6675ec64a20
Martirossyan Ararat <myfailemtions@gmail.com>
2020-01-11 17:44:18 +0100

    Updates

---
+++ precache-manifest.6abe288291efd30fa272dad351543c97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c1f7ee7443685f296dc15dcdf39ba12",
+    "url": "/movieology/index.html"
+  },
+  {
+    "revision": "dee88c356b7c66c3dbba",
+    "url": "/movieology/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "8107caaca38da8248fc5",
+    "url": "/movieology/static/css/main.3d19c9ef.chunk.css"
+  },
+  {
+    "revision": "dee88c356b7c66c3dbba",
+    "url": "/movieology/static/js/2.38a20179.chunk.js"
+  },
+  {
+    "revision": "8a14dcea7ad8b593c1e83c11f7ba1a89",
+    "url": "/movieology/static/js/2.38a20179.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8107caaca38da8248fc5",
+    "url": "/movieology/static/js/main.1b849941.chunk.js"
+  },
+  {
+    "revision": "f355bdc9047d2547751f",
+    "url": "/movieology/static/js/runtime-main.1a0add10.js"
+  }
+]);
